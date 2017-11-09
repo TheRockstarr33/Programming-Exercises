27b4a6d42d1477f9cad90606a69e72413a73dce4
TheRockstarr33 <drockstarr33@yahoo.com>
2017-11-09 16:08:33 +0400

    Added Armstrong number

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void printAllOptions() {
+        System.out.println("1. Even or Odd");
+        System.out.println("2. Prime Numbers");
+        System.out.println("3. Palindromes");
+        System.out.println("4. Power of 2");
+        System.out.println("5. Sort Integer Arrays");
+        System.out.println("6. Armstrong Numbers");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Please enter the number for your function. ");
+        System.out.println("Please enter the number for your function. Options are: ");
+        printAllOptions();
+
         Scanner scanner = new Scanner(System.in);
         int firstOption = Integer.parseInt(scanner.nextLine());
 
@@ -15,5 +26,6 @@ public class Main {
         else if(firstOption==3) Palindrome.palindrome();
         else if(firstOption==4) Power.isPowerOf2();
         else if(firstOption==5) SortArray.sortIntArray();
+        else if(firstOption==6) Armstrong.isArmNum();
     }
 }
