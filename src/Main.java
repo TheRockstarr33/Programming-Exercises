05de295cdb6cfc203aa16f02a6e3f03fccf6a204
Skye <pewpewdjent3@gmail.com>
2021-06-17 14:02:56 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//Java programming exercises, by David Ryan
+//Java programming exercises
 //Link to resource: http://www.java67.com/2013/01/10-programming-questions-and-exercises.html
 
 import java.util.Scanner;
